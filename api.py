fb675a4992747ea73aafa0d5bbdbefd908b0d4c5
mario_rosales <mariocrosl@gmail.com>
2020-04-12 15:06:10 +0200

    add api

--- api.py
+++ api.py
@@ -1,14 +1,55 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import humidity
+import temperature
+import time
+import json
+import numpy
+
 
 app = Flask(__name__)
 api = Api(app)
 
+
+def prediction_service(periods):
+
+    humidity_values = humidity.predict_humidity(periods)
+    temperature_values = temperature.predict_temperature(periods)
+
+    array = numpy.array(temperature_values)
+    temperature_values_celcius = array - 273.15
+
+    t0 = time.time()
+    current_time = time.strftime("%H:%M",time.localtime(t0))
+
+    list = [{"hour":current_time,"temp":temperature_values_celcius[0],"hum":humidity_values[0]}]
+    for x in range(1, periods):
+        tn = t0 + 60*60 * x
+        ntime = time.strftime("%H:%M",time.localtime(tn))
+        list.append({"hour": ntime,"temp":temperature_values_celcius[x],"hum":humidity_values[x]})
+            
+    return list
+
 class Prediction24(Resource):
     def get(self):
-        return [{"hour":"13:10","temp":"31.20","hum":"86.30"}]
+        json = prediction_service(24)
+        return json
 
-api.add_resource(Prediction24, '/servicio/v1/prediccion/24horas/')
+api.add_resource(Prediction24, '/servicio/v1/prediccion/24horas')
+
+class Prediction48(Resource):
+    def get(self):
+        json = prediction_service(48)
+        return json
+
+api.add_resource(Prediction48, '/servicio/v1/prediccion/48horas')
+
+class Prediction72(Resource):
+    def get(self):
+        json = prediction_service(24)
+        return json
+
+api.add_resource(Prediction72, '/servicio/v1/prediccion/72horas/')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ humidity.py
@@ -0,0 +1,41 @@
+from statsmodels.tsa.arima_model import ARIMA
+import pandas as pd
+import pmdarima as pm
+import pymysql
+
+
+def predict_humidity(periods):
+
+    # Open database connection
+    db = pymysql.connect("localhost","root","admin","admin" )
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM admin.historical_data")
+
+    # Fetch a single row using fetchone() method.
+    db_data = cursor.fetchall()
+
+    db.close()    
+    
+    df = pd.DataFrame(db_data, columns =['id','measure_date', 'temperature', 'humidity'])
+    #df = pd.read_csv('humidity_min_clean.csv', names=['datetime','san_francisco'], header=0)
+    
+    model = pm.auto_arima(df.humidity, start_p=1, start_q=1,
+                      test='adf',       # use adftest to find optimal 'd'
+                      max_p=3, max_q=3, # maximum p and q
+                      m=1,              # frequency of series
+                      d=None,           # let model determine 'd'
+                      seasonal=False,   # No Seasonality
+                      start_P=0, 
+                      D=0, 
+                      trace=True,
+                      error_action='ignore',  
+                      suppress_warnings=True, 
+                      stepwise=True)
+    # Forecast
+    n_periods = periods # Days
+    fc, confint = model.predict(n_periods=n_periods, return_conf_int=True)
+    # fc contains the forecasting for the next 24 hours.
+    return fc
